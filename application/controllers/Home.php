60ce31c663481bfbeab7f1ee8d9877104797c355
Nihar Ranjan Bhattacharya <nihar@redappletech.com>
2024-04-18 17:54:02 +0530

    betgames game launch

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -214,6 +214,85 @@ class Home extends MY_Controller
 		$ip = $_SERVER['REMOTE_ADDR'] ?: ($_SERVER['HTTP_X_FORWARDED_FOR'] ?: $_SERVER['HTTP_CLIENT_IP']);
 		return $ip;
 	}
+
+	public function betgameslaunch()
+	{
+		$data = array();
+		$params = $this->input->get();
+		$provider_id = $this->input->get('pid', TRUE);
+		$game_code  = $this->input->get('gid', TRUE);
+        $ticket  = $this->input->get('ticket', TRUE);
+        $client_id = $this->input->get('cid', TRUE);
+		$language = $this->input->get('language', TRUE);
+
+		$chkuser_details = $this->Home_model->getUserDtlsByToken('PlayerToken',$ticket,$provider_id,$client_id);
+		if(empty($chkuser_details))
+		{
+			$resultarr = json_encode([
+				"status" => "error",
+				"error"=> [
+				  "scope"=> "user",
+				  "no_refund"=>"1",
+				  "message"=> "Token mismatched!"
+				]
+			]);
+			echo $resultarr; die;
+		}
+
+		$gamedetail = $this->Home_model->getGameDetailsbyCode($game_code, $provider_id);
+		if(empty($gamedetail))
+		{
+			$resultarr = json_encode([
+				"status" => "error",
+				"code"=> "1007",
+				"message"=>"Game not found!"
+			]);
+
+			echo $resultarr; die;
+		}
+
+		$provider_params = $this->Home_model->get_provider_params($client_id, $provider_id);
+		$pparam = array();
+		if (!empty($provider_params)) 
+		{
+			foreach ($provider_params as $provider_params) 
+			{
+				$pparam[$provider_params['field_key']] = $provider_params['field_value'];
+			}
+			
+			//$data['stagecheck'] = $this->staging_check;
+		}
+		//print_r($pparam);
+		$clienturl = $pparam['clienturl'];
+		$partnercode = $pparam['partnerCode'];
+		$apiurl = $pparam['apiurl'];
+		$wsurl = $pparam['wsurl'];
+		if($provider_id == 65){
+
+			if($game_code == 'lobby'){
+	  
+			  $gameTokenUrl   =   "{$clienturl}/auth?partnerCode={$partnercode}&token={$ticket}&locale={$language}";
+	  
+			}else{
+	  
+			  $gameTokenUrl   =   "{$clienturl}/auth?partnerCode={$partnercode}&token={$ticket}&locale={$language}&gameId={$game_code}";
+	  
+			}
+
+		   }else{
+	  
+			$gameTokenUrl   =   "{$clienturl}/#/?apiUrl={$apiurl}&wsUrl={$wsurl}&partnerCode={$partnercode}&token={$ticket}&locale={$language}&gameId={$game_code}";
+	  
+		   }
+
+		//echo $gameTokenUrl; die;
+
+		$data['launchUrl'] =  $gameTokenUrl;
+		//$data['query'] = $query ; 
+		
+		
+		$this->commonLayoutView('betgameslaunch', $data, true);
+	}
 	/* public function index()
 	{
 		if ($this->session->userdata('session_data')) {
---
+++ application/views/betgameslaunch.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title></title>
+</head>
+<body>
+<div id="iframe-content" style="margin: 0 auto; padding-bottom: 100px">
+     <iframe
+        frameborder="0"
+        allow="fullscreen" 
+        scrolling="no"
+        src="<?php echo $launchUrl;?>"
+        style="min-width: 100%; height: 1410px;">
+     </iframe>
+</div>
+</body>
+</html>
