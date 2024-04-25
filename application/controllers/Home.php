b807346713776e5efa00a838d5c101d4fd16fc5c
Nihar Ranjan Bhattacharya <nihar@redappletech.com>
2024-04-25 15:04:52 +0530

    bug fix

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -229,7 +229,7 @@ class Home extends MY_Controller
 		$providerdetail = $this->Home_model->getproviderdetails($provider_id);
 		$parentprovider = !empty($providerdetail['parent_id'])?$providerdetail['parent_id']:$provider_id;
 
-		$chkuser_details = $this->Home_model->getUserDtlsByToken('PlayerToken',$ticket,$parentprovider,$client_id);
+		$chkuser_details = $this->Home_model->getUserDtlsByToken('PlayerToken',$ticket,$provider_id,$client_id);
 		if(empty($chkuser_details))
 		{
 			$resultarr = json_encode([
@@ -243,7 +243,7 @@ class Home extends MY_Controller
 			echo $resultarr; die;
 		}
 
-		$gamedetail = $this->Home_model->getGameDetailsbyCode($game_code, $parentprovider);
+		$gamedetail = $this->Home_model->getGameDetailsbyCode($game_code, $provider_id);
 
 		if(empty($gamedetail))
 		{
