7cafaa43a945ae1755a056772086d9e06e12956f
Arindam Biswas <arindam@redappletech.com>
2024-06-19 00:57:53 +0530

    PGsoft game launch issue fixing(#2)

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -182,11 +182,11 @@ class Home extends MY_Controller
       curl_setopt($ch, CURLOPT_POSTFIELDS, $body_params_encoded);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       $response = curl_exec($ch);
-		echo $url;
-		echo $body_params_encoded;
-		echo '<pre>'; print_r($headers);
-		echo '<pre>'; print_r($response);
-		die;
+		// echo $url;
+		// echo $body_params_encoded;
+		// echo '<pre>'; print_r($headers);
+		// echo '<pre>'; print_r($response);
+		// die;
 	 $result = array(); 
 	  if($response === false)
       {
